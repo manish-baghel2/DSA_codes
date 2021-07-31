a83932091e14e1423f17413d72ed0c66062fae09
manish-baghel2 <manish_11914130@nitkkr.ac.in>
2021-07-31 15:50:29 +0530

    longest-substring-non-repititive.cpp

---
+++ longest-substring-non-repeatitive.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;cin>>s;
+    vector<int> dict(256, -1);
+    int start=-1, maxlen = 0;
+    for(int i=0;i<s.size();i++)
+    {
+        if(dict[s[i]] > start)
+        {
+            start = dict[s[i]];
+        }
+        dict[s[i]] = i;
+        maxlen = max(maxlen, i-start);
+    }
+    cout<<maxlen;
+}
