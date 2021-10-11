f3405e8f25f7730b3f8dd21b3031eb0cf4b3d742
manish-baghel1 <manish_11914130@nitkkr.ac.in>
2021-10-11 23:18:18 +0530

    b

--- knapsack1.cpp
+++ knapsack1.cpp
@@ -10,20 +10,10 @@ int max(int a, int b)
 int knapSack(int w, int wt[], int val[], int n)
 {
     vector<vector<int>> k(n+1, vector<int>(w+1));
-=======
-#include <bits/stdc++.h>
-using namespace std;
-
-int knapsack(int w, int wt*, int val*, int n)
-{
-    vector<vector<int>> k( n+1, vector<int>(w+1) );
-
->>>>>>> e1680fff59a7f5acee512525d7a19a73b7fd9ae3
     for(int i=0;i<n+1;i++)
     {
         for(int j=0;j<w+1;j++)
         {
-<<<<<<< HEAD
             if(i==0||j==0)
             {
                 k[i][j] = 0;
@@ -37,17 +27,6 @@ int knapsack(int w, int wt*, int val*, int n)
         }
     }
     return k[n][w];
-=======
-            if(i==0 || j==0)
-            {
-                k[i][j] = 0;
-            }else if(wt[i] > w)
-            {
-                k[n][w] = 
-            }
-        }
-    }
->>>>>>> e1680fff59a7f5acee512525d7a19a73b7fd9ae3
 }
 
 int main()
@@ -56,16 +35,8 @@ int main()
     int wt[] = { 10, 20, 30 };
     int W = 50;
     int n = sizeof(val) / sizeof(val[0]);
-     
-<<<<<<< HEAD
     cout << knapSack(W, wt, val, n) << endl;
      
     return 0;
 }
-=======
-    cout << knapSack(W, wt, val, n);
-     
-    return 0;
-}
 
->>>>>>> e1680fff59a7f5acee512525d7a19a73b7fd9ae3
